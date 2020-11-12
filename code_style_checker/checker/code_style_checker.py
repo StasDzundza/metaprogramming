00d7bdf9242758ef6d0187e4c58c1ff346bca9cb
Stanislav Dzundza <stas.dzundza@gmail.com>
2020-11-13 01:43:01 +0200

    fix logging. add support for verify mode

--- code_style_checker/checker/code_style_checker.py
+++ code_style_checker/checker/code_style_checker.py
@@ -2,6 +2,7 @@ import os
 from utils.logger import *
 from lexer.lexer import *
 from checker.name_formatter import *
+from WorkingMode import *
 
 
 class ErrorType(Enum):
@@ -11,7 +12,9 @@ class ErrorType(Enum):
     EnumMemberNameError = 3,
     MacrosNameError = 4,
     FileNameError = 5,
-    ConstMemberError = 6
+    ConstVarError = 6,
+    NamespaceNameError = 7,
+    CommonVarNameError = 8
 
 
 class CodeStyleChecker:
@@ -20,10 +23,7 @@ class CodeStyleChecker:
         self.logger = Logger("code_style_fixing.log", "code_style_verification.log")
         self.user_type_keywords = {"class", "struct", "enum"}
 
-    def check_code_style(self, file_path):
-        pass
-
-    def fix_code_style(self, file_path):
+    def fix_code_style(self, file_path, working_mode: WorkingMode):
         lexer = Lexer()
         with open(file_path, 'r', encoding="utf-8") as file:
             cpp_code = file.read()
@@ -41,25 +41,16 @@ class CodeStyleChecker:
                 if i + 1 < len(tokens) and tokens[i + 1].value == "::":  # namespace or type names before ::
                     if cur_token.value in defined_namespaces or self.mode != '-f':
                         cur_output = format_common_var_name(cur_token.value)
-                        if cur_output != cur_token.value:
-                            self.logger.log_style_fix(file_path, cur_token.line,
-                                                      "fixed namespace name {} to {}".format(cur_token.value,
-                                                                                             cur_output))
+                        self.log_if_need(file_path, cur_token, cur_output, ErrorType.NamespaceNameError, working_mode)
                     elif cur_token in defined_type_names or self.mode != '-f':
                         cur_output = format_type_name(cur_token.value)
-                        if cur_output != cur_token.value:
-                            self.logger.log_style_fix(file_path, cur_token.line,
-                                                      "fixed type name {} to {}".format(cur_token.value,
-                                                                                        cur_output))
+                        self.log_if_need(file_path, cur_token, cur_output, ErrorType.TypeNameError, working_mode)
                     else:
                         cur_output = cur_token.value
                 elif len(token_stack) > 0 and token_stack[-1] == "define":  # macro
                     token_stack.pop()  # remove define from stack
                     cur_output = format_macro_name(cur_token.value)
-                    if cur_output != cur_token.value:
-                        self.logger.log_style_fix(file_path, cur_token.line,
-                                                  "fixed macro name {} to {}".format(cur_token.value,
-                                                                                     cur_output))
+                    self.log_if_need(file_path, cur_token, cur_output, ErrorType.MacrosNameError, working_mode)
                 elif i + 1 < len(tokens) and tokens[i + 1].value == '(':  # function
                     prev_token = self.get_prev_token(tokens, i)
                     if prev_token is not None and (
@@ -67,33 +58,23 @@ class CodeStyleChecker:
                             prev_token.value in ('*', '&')):  # func declaration
                         defined_functions.append(cur_token.value)
                         cur_output = format_func_name(cur_token.value)
-                        if cur_output != cur_token.value:
-                            self.logger.log_style_fix(file_path, cur_token.line,
-                                                      "fixed function name {} to {}".format(cur_token.value,
-                                                                                            cur_output))
+                        self.log_if_need(file_path, cur_token, cur_output, ErrorType.FunctionNameError, working_mode)
                     else:  # func invocation
                         if cur_token.value in defined_functions or self.mode != '-f':
                             cur_output = format_func_name(cur_token.value)
-                            if cur_output != cur_token.value:
-                                self.logger.log_style_fix(file_path, cur_token.line,
-                                                          "fixed function name {} to {}".format(cur_token.value,
-                                                                                                cur_output))
+                            self.log_if_need(file_path, cur_token, cur_output, ErrorType.FunctionNameError, working_mode)
                         else:
                             cur_output = cur_token.value  # defined in other file
                 elif len(token_stack) > 0 and token_stack[-1] in ("class", "struct", "enum", "typename"):
                     # class,struct,enum, template definition
                     defined_type_names.append(cur_token.value)
                     cur_output = format_type_name(cur_token.value)
-                    if cur_output != cur_token.value:
-                        self.logger.log_style_fix(file_path, cur_token.line,
-                                                  "fixed type name {} to {}".format(cur_token.value, cur_output))
+                    self.log_if_need(file_path, cur_token, cur_output, ErrorType.TypeNameError, working_mode)
                 elif len(token_stack) > 0 and token_stack[-1] in ACCESS_MODIFIERS:  # inheritance
                     token_stack.pop()  # remove access modifier from stack
                     if cur_token.value in defined_type_names or self.mode != '-f':
                         cur_output = format_type_name(cur_token.value)
-                        if cur_output != cur_token.value:
-                            self.logger.log_style_fix(file_path, cur_token.line,
-                                                      "fixed type name {} to {}".format(cur_token.value, cur_output))
+                        self.log_if_need(file_path, cur_token, cur_output, ErrorType.TypeNameError, working_mode)
                     else:
                         cur_output = cur_token.value
                 elif len(token_stack) > 1 and token_stack[-2] == "class":  # class member TODO check for func decl
@@ -101,23 +82,16 @@ class CodeStyleChecker:
                     if prev_token is not None and (prev_token.token_name in (TokenName.IDENTIFIER, TokenName.DATA_TYPE)
                                                    or prev_token.value in ('*', ',', '&')):
                         cur_output = format_class_var_name(cur_token.value)
-                        if cur_output != cur_token.value:
-                            self.logger.log_style_fix(file_path, cur_token.line,
-                                                      "fixed class var name {} to {}".format(cur_token.value,
-                                                                                             cur_output))
+                        self.log_if_need(file_path, cur_token, cur_output, ErrorType.ClassMemberNameError, working_mode)
                     else:
                         cur_output = cur_token.value
                 elif len(token_stack) > 1 and token_stack[-2] == "enum":  # enum member
                     cur_output = format_const_var_name(cur_token.value)
-                    if cur_output != cur_token.value:
-                        self.logger.log_style_fix(file_path, cur_token.line,
-                                                  "fixed enum var name {} to {}".format(cur_token.value, cur_output))
+                    self.log_if_need(file_path, cur_token, cur_output, ErrorType.EnumMemberNameError, working_mode)
                 elif len(token_stack) > 0 and token_stack[-1] == "namespace":
                     defined_namespaces.append(cur_token.value)
                     cur_output = format_common_var_name(cur_token.value)
-                    if cur_output != cur_token.value:
-                        self.logger.log_style_fix(file_path, cur_token.line,
-                                                  "fixed namespace name {} to {}".format(cur_token.value, cur_output))
+                    self.log_if_need(file_path, cur_token, cur_output, ErrorType.NamespaceNameError, working_mode)
                 else:
                     cur_output = cur_token.value
             elif cur_token.token_name == TokenName.KEYWORD:
@@ -169,13 +143,59 @@ class CodeStyleChecker:
                 cur_output = cur_token.value
             else:
                 cur_output = cur_token.value
+            if working_mode == WorkingMode.VerifyMode:
+                cur_output = cur_token.value
             output = output + cur_output
             i += 1
-        # formatted_file_name = os.path.basename(file_path)
-        # if self.mode != '-f':
-        formatted_file_name = format_file_name(os.path.basename(file_path))
-        dir_name = os.path.dirname(file_path)
-        self.save_text_in_file(os.path.join(dir_name, formatted_file_name), output)
+        if working_mode == WorkingMode.FixMode:
+            # formatted_file_name = os.path.basename(file_path)
+            # if self.mode != '-f':
+            formatted_file_name = format_file_name(os.path.basename(file_path))
+            dir_name = os.path.dirname(file_path)
+            self.save_text_in_file(os.path.join(dir_name, formatted_file_name), output)
+
+    def log_if_need(self, file_path, cur_token, cur_output, error_type, mode):
+        if cur_output != cur_token.value:
+            if error_type == ErrorType.NamespaceNameError:
+                self.logger.log(file_path, cur_token.line,
+                                "fixed namespace name {} to {}".format(cur_token.value, cur_output), error_type,
+                                "Incorrect namespace name", mode)
+            elif error_type == ErrorType.ClassMemberNameError:
+                self.logger.log(file_path, cur_token.line,
+                                "fixed class member name {} to {}".format(cur_token.value, cur_output), error_type,
+                                "Incorrect class member name", mode)
+            elif error_type == ErrorType.ConstVarError:
+                self.logger.log(file_path, cur_token.line,
+                                "fixed const var name {} to {}".format(cur_token.value, cur_output), error_type,
+                                "Incorrect const var name", mode)
+            elif error_type == ErrorType.EnumMemberNameError:
+                self.logger.log(file_path, cur_token.line,
+                                "fixed enum member name {} to {}".format(cur_token.value, cur_output), error_type,
+                                "Incorrect enum member name", mode)
+            elif error_type == ErrorType.FileNameError:
+                self.logger.log(file_path, cur_token.line,
+                                "fixed file name {} to {}".format(cur_token.value, cur_output), error_type,
+                                "Incorrect file name", mode)
+            elif error_type == ErrorType.FunctionNameError:
+                self.logger.log(file_path, cur_token.line,
+                                "fixed func name {} to {}".format(cur_token.value, cur_output), error_type,
+                                "Incorrect func name", mode)
+            elif error_type == ErrorType.TypeNameError:
+                self.logger.log(file_path, cur_token.line,
+                                "fixed type name {} to {}".format(cur_token.value, cur_output), error_type,
+                                "Incorrect type name", mode)
+            elif error_type == ErrorType.MacrosNameError:
+                self.logger.log(file_path, cur_token.line,
+                                "fixed macro name {} to {}".format(cur_token.value, cur_output), error_type,
+                                "Incorrect macro name", mode)
+            elif error_type == ErrorType.CommonVarNameError:
+                self.logger.log(file_path, cur_token.line,
+                                "fixed common var name {} to {}".format(cur_token.value, cur_output), error_type,
+                                "Incorrect common var name", mode)
+            else:
+                self.logger.log(file_path, cur_token.line,
+                                "fixed {} to {}".format(cur_token.value, cur_output), error_type,
+                                "Unknown error", mode)
 
     def run_for_project(self, mode, project_path):
         files = []
@@ -204,9 +224,9 @@ class CodeStyleChecker:
 
     def run_for_file(self, mode, file_path):
         if mode in ("--verify", "-v"):
-            self.check_code_style(file_path)
+            self.fix_code_style(file_path, WorkingMode.VerifyMode)
         elif mode in ("--fix", "-f"):
-            self.fix_code_style(file_path)
+            self.fix_code_style(file_path, WorkingMode.FixMode)
         else:
             print("Incorrect mode")
 
--- code_style_checker/utils/logger.py
+++ code_style_checker/utils/logger.py
@@ -1,3 +1,6 @@
+from WorkingMode import *
+
+
 class Logger:
     def __init__(self, fix_log_file_name, verify_log_file_name):
         self.fix_log_file = open(fix_log_file_name, 'a', encoding='utf-8')
@@ -6,6 +9,12 @@ class Logger:
     def __del__(self):
         self.fix_log_file.close()
         self.verify_log_file.close()
+
+    def log(self, file_path, line_num, modification_description, error_code, error_message, mode):
+        if mode == WorkingMode.VerifyMode:
+            self.log_style_error(file_path, line_num, error_code, error_message)
+        else:
+            self.log_style_fix(file_path, line_num, modification_description)
 
     def log_style_fix(self, file_path, line_num, modification_description):
         message = "{}: {} - {}\n".format(file_path, line_num, modification_description)
